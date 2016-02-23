abfc6385ee2b89dc1dffb3bddbda3a5e5d7fb963
rcorbeil <rcorbeil@softstackfactory.com>
2016-02-23 01:17:12 +0000

    before auth

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('./models/Posts');
+require('./models/Comments');
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
